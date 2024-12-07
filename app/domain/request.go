eec6ca5474b63def2e1825b59d3b095054ec54b9
superkruger <superkruger@gmail.com>
2024-12-07 21:01:08 +0100

    add indexes

--- app/domain/request.go
+++ app/domain/request.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-var indexedTags = map[string]struct{}{"e": {}, "p": {}, "a": {}, "d": {}}
+var indexedTags = map[string]struct{}{"e": {}, "p": {}, "a": {}, "d": {}, "k": {}, "l": {}}
 
 type Request struct {
 	ID       string    `json:"id" bson:"id"`
